d09b534dbd2c02d9c6b67c5646075743dc7eaede
Jwala Sowmika <jwalasowmika@gmail.com>
2021-04-30 11:36:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, json, redirect
 
 app = Flask(__name__)
 
-@app.route(constants.HOME_PATH)
+@app.route('/')
 def index():
     return "Hello World"
 
